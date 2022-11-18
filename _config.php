7265b55f53fd4f48d77c70917033834498751dee
prameshharshana3 <prameshharshana2@gmail.com>
2022-11-18 10:59:59 +0530

    fixed module path

--- _config.php
+++ _config.php
@@ -6,8 +6,8 @@ use SheaDawson\Blocks\Controllers;
 
 //define global path to Components' root folder
 if (!defined('BLOCKS_DIR')) {
-	define('BLOCKS_DIR', rtrim(basename(dirname(__FILE__))));
+	define('BLOCKS_DIR', 'vendor/sheadawson/' . rtrim(basename(dirname(__FILE__))));
 }
 
-Config::modify()->update(LeftAndMain::class, 'extra_requirements_javascript', array(BLOCKS_DIR.'/javascript/blocks-cms.js'));
-Config::modify()->update(BlockAdmin::class, 'menu_icon', BLOCKS_DIR.'/images/blocks.png');
+Config::modify()->update(LeftAndMain::class, 'extra_requirements_javascript', [BLOCKS_DIR. '/javascript/blocks-cms.js']);
+Config::modify()->update(BlockAdmin::class, 'menu_icon', BLOCKS_DIR. '/images/blocks.png');
