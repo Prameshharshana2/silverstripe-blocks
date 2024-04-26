394f88029d96baa72fe72a22ac9a60ce83e9b427
3Dgoo <michael@3dgoo.com>
2024-04-26 23:52:57 +0930

    Update _config.php

--- _config.php
+++ _config.php
@@ -2,12 +2,12 @@
 
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Admin\LeftAndMain;
-use SheaDawson\Blocks\Controllers;
+use SheaDawson\Blocks\Controllers\BlockAdmin;
 
 //define global path to Components' root folder
 if (!defined('BLOCKS_DIR')) {
 	define('BLOCKS_DIR', 'vendor/sheadawson/' . rtrim(basename(dirname(__FILE__))));
 }
 
-Config::modify()->update(LeftAndMain::class, 'extra_requirements_javascript', [BLOCKS_DIR. '/javascript/blocks-cms.js']);
-Config::modify()->update(BlockAdmin::class, 'menu_icon', BLOCKS_DIR. '/images/blocks.png');
+Config::modify()->set(LeftAndMain::class, 'extra_requirements_javascript', [BLOCKS_DIR. '/javascript/blocks-cms.js']);
+Config::modify()->set(BlockAdmin::class, 'menu_icon', BLOCKS_DIR. '/images/blocks.png');
