316700bb2586a72136a0c74526215ba8f5cf95dd
Michael Nowina-Krowicki <michael@3dgoo.com>
2024-06-18 08:59:56 +0930

    Silverstripe 4 update

--- _config.php
+++ _config.php
@@ -1,13 +1,1 @@
 <?php
-
-use SilverStripe\Core\Config\Config;
-use SilverStripe\Admin\LeftAndMain;
-use SheaDawson\Blocks\Controllers\BlockAdmin;
-
-//define global path to Components' root folder
-if (!defined('BLOCKS_DIR')) {
-	define('BLOCKS_DIR', 'vendor/sheadawson/' . rtrim(basename(dirname(__FILE__))));
-}
-
-Config::modify()->set(LeftAndMain::class, 'extra_requirements_javascript', [BLOCKS_DIR. '/javascript/blocks-cms.js']);
-Config::modify()->set(BlockAdmin::class, 'menu_icon', BLOCKS_DIR. '/images/blocks.png');
